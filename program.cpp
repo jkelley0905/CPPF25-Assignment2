0f7e7f88bf7bb0f786369c6c6fb5e015c1186ef3
jkelley0905 <jkelley0905@gmail.com>
2025-11-05 17:21:50 -0500

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -1,16 +1,25 @@
 #include "bmp.hpp"
 #include <iostream>
 #include <time.h>
+//b is the width of the lines of the rectangle
+//it draws top and bottom first
+//it draws left and right second
 void draw_rectangle(BMP &bmp,int x, int y, int l, int h, int b, color c ){
     for(int i=0;i<=l;i++){
         for(int ii=0;ii<=b;ii++){
-        bmp.set_pixel(i+x, y+ii, c.r,c.g,c.b);
-        bmp.set_pixel(i+x, y+h-ii, c.r, c.g,c.b);
+            //top line
+            bmp.set_pixel(i+x, y+ii, c.r,c.g,c.b);
+
+            //botom line
+            bmp.set_pixel(i+x, y+h-ii, c.r, c.g,c.b);
         }
     }
     for(int j=0;j<=h;j++){
         for(int ii=0;ii<=b;ii++){
+            //left line
             bmp.set_pixel(x+ii, y+j, c.r,c.g,c.b);
+
+            //right line
             bmp.set_pixel(x+l-ii, y+j,c.r, c.g,c.b);
         }
     }
