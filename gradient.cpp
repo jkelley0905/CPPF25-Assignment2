6a8812f5e835dd2f43167c66e5c04d42c4c525e7
jkelley0905 <jkelley0905@gmail.com>
2025-11-05 18:03:03 -0500

    Update gradient.cpp

--- gradient.cpp
+++ gradient.cpp
@@ -6,10 +6,10 @@ void add_gradient (BMP &bmp, color a, color b){
     int width = bmp.get_width();
     int height = bmp.get_height();
 
-    float gradient = height / 100; 
+    float gradient = (float) (height / 100); 
 
-    for (int i = 0; i <= height, i++) {
-        for (int j = 0; j <= width, j++) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
             bmp.set_pixel(j, i, ((a.r * (100 - (gradient * i)))+(b.r * (gradient * i))),((a.g * (100 - (gradient * i)))+(b.g * (gradient * i))),((a.b * (100 - (gradient * i)))+(b.b * (gradient * i))));
         }
     }
@@ -17,7 +17,7 @@ void add_gradient (BMP &bmp, color a, color b){
 
 int main () {
     BMP bmp (1000,500);
-    color a (50,50,50);
+    color a (0,0,0);
     color b (0, 0, 255);
 
     add_gradient (bmp, a, b);
@@ -25,3 +25,6 @@ int main () {
     bmp.write("gradient.bmp"); // Save the image to a file
 
     return 0;
+}
+
+    
